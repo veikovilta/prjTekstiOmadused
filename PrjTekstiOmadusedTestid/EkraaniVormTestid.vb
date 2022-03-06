c9519bec088c8a81e695f49cd9fe2e3d46e3704f
risto <risto@PC>
2022-03-06 22:53:27 +0200

    Muudetud solutionis projekti asukoht

---
+++ PrjTekstiOmadusedTestid/EkraaniVormTestid.vb
@@ -0,0 +1,15 @@
+﻿Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+
+<TestClass()> Public Class EkraaniVormTestid
+
+
+    ' <TestMethod()>
+    ' Public Sub KontrolliPalindroomi()
+    'Dim pooraja As New PrjTekstiOmadused.EkraaniVorm
+    'Dim vastus = pooraja.PalindroomVastus(True)
+    '     Assert.IsTrue(vastus = "Jah")
+    ' End Sub
+
+End Class
---
+++ PrjTekstiOmadusedTestid/My Project/AssemblyInfo.vb
@@ -0,0 +1,18 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("PrjTekstiOmadusedTestid")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("PrjTekstiOmadusedTestid")>
+<Assembly: AssemblyCopyright("Copyright ©  2022")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+<Assembly: Guid("465b6e6f-1ce5-4d7e-8ec8-37b53b3fad37")>
+
+' <Assembly: AssemblyVersion("1.0.*")> 
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
